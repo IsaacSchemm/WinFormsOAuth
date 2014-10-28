43c5d23cb87d6c28635df6b8ae4823179b3749bc
IsaacSchemm <isaacschemm@gmail.com>
2014-10-27 21:15:17 -0500

    Rename/resize form and add README

--- WinFormsWebBrowserOAuth/LoginForm.Designer.cs
+++ WinFormsWebBrowserOAuth/LoginForm.Designer.cs
@@ -28,51 +28,54 @@ namespace WebBrowserOAuth
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
-            this.browser = new System.Windows.Forms.WebBrowser();
-            this.addressTextBox = new System.Windows.Forms.TextBox();
-            this.SuspendLayout();
-            // 
-            // browser
-            // 
-            this.browser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.browser.Location = new System.Drawing.Point(0, 26);
-            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
-            this.browser.Name = "browser";
-            this.browser.Size = new System.Drawing.Size(864, 561);
-            this.browser.TabIndex = 0;
-            this.browser.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.webBrowser1_Navigating);
-            this.browser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
-            this.browser.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.browser_Navigated);
-            // 
-            // addressTextBox
-            // 
-            this.addressTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.addressTextBox.Location = new System.Drawing.Point(7, 2);
-            this.addressTextBox.Name = "addressTextBox";
-            this.addressTextBox.Size = new System.Drawing.Size(840, 20);
-            this.addressTextBox.TabIndex = 1;
-            this.addressTextBox.TabStop = false;
-            this.addressTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.addressTextBox_KeyPress);
-            // 
-            // LoginForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(864, 587);
-            this.Controls.Add(this.addressTextBox);
-            this.Controls.Add(this.browser);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "LoginForm";
-            this.ShowInTaskbar = false;
-            this.Text = "Linkedin Connect";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+			this.browser = new System.Windows.Forms.WebBrowser();
+			this.addressTextBox = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// browser
+			// 
+			this.browser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.browser.Location = new System.Drawing.Point(0, 32);
+			this.browser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.browser.MinimumSize = new System.Drawing.Size(27, 25);
+			this.browser.Name = "browser";
+			this.browser.Size = new System.Drawing.Size(622, 561);
+			this.browser.TabIndex = 0;
+			this.browser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+			this.browser.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.browser_Navigated);
+			this.browser.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.webBrowser1_Navigating);
+			// 
+			// addressTextBox
+			// 
+			this.addressTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.addressTextBox.Location = new System.Drawing.Point(9, 2);
+			this.addressTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.addressTextBox.Name = "addressTextBox";
+			this.addressTextBox.Size = new System.Drawing.Size(589, 22);
+			this.addressTextBox.TabIndex = 1;
+			this.addressTextBox.TabStop = false;
+			this.addressTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.addressTextBox_KeyPress);
+			// 
+			// LoginForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(622, 593);
+			this.Controls.Add(this.addressTextBox);
+			this.Controls.Add(this.browser);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "LoginForm";
+			this.ShowInTaskbar = false;
+			this.Text = "Log in";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
