f07fde6ff5d200b1204b72aa2897ea605e1a769f
IsaacSchemm <isaacschemm@gmail.com>
2014-11-11 19:49:17 -0600

    Rename namespace and project and update assembly info

--- WinFormsOAuth/Properties/Resources.Designer.cs
+++ WinFormsOAuth/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WinFormsWebBrowserOAuth.Properties {
+namespace WinFormsOAuth.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace WinFormsWebBrowserOAuth.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WinFormsWebBrowserOAuth.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WinFormsOAuth.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- WinFormsOAuth/Properties/Settings.Designer.cs
+++ WinFormsOAuth/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WinFormsWebBrowserOAuth.Properties {
+namespace WinFormsOAuth.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
